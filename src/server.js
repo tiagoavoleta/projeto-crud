6c55b4cf31f9fc1b1c6be356ec4ddf41b014914e
Tiago Tavares <tiagoavoleta@hotmail.com>
2023-07-12 10:19:32 -0300

    adicionando estrutura inicial de pastas

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 // habilita server para receber dados via post (formulário)
-app.use(express.urlenconded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
 // rotas
 app.get('/', (req, res) => {
